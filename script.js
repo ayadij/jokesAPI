cb19813e893b034a84232232aa450131532771fc
Aya <aya@safeandreliablecare.com>
2020-02-21 01:49:56 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("joke").addEventListener("click", function(event) {
+  event.preventDefault();
+
+  const url =
+    "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist";
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      let results = "";
+      results += json.joke;
+      console.log(results);
+      // document.getElementById("jumbo").innerHTML = results;
+    });
+});
