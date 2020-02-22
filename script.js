c4ce31c12cbc209a2fde5a8c76465da5b7d1a949
Aya <aya@safeandreliablecare.com>
2020-02-21 20:41:57 -0700

    adjusted the script to handle error of undefined joke

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ document.getElementById("joke").addEventListener("click", function(event) {
     })
     .then(function(json) {
       let results = json.joke;
+      if (results == undefined) {
+        results =
+          "<em>Hmmmm... I am still thinking of a joke... Try pushing the button again!</em>";
+      }
       document.getElementById("jumbo").innerHTML = results;
     });
 });
